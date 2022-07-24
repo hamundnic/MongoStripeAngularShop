4b22d90e24aaa176d808739ef2446df4e01c7018
hamundnic <hamundnic@gmail.com>
2022-07-24 12:24:28 +0200

    first comic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminModule } from './@admin/pages/admin.module';
+import { PublicModule } from './@public/pages/public.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AdminModule,
+    PublicModule,
+
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
